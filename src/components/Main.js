7553a7b0951ec2c77bdeb26bf74241c38aceaf2c
a1abed1a <abedshehadeh1@gmail.com>
2021-09-15 18:33:34 +0300

    done lab 4

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Beast from "./Beast";
 import Data from "./Data.json";
-import { Button, Modal } from 'react-bootstrap'
+import { Button, Modal, Form } from 'react-bootstrap'
 
 class Main extends React.Component {
 
@@ -9,6 +9,7 @@ class Main extends React.Component {
         super(props);
         this.state = {
             modal: false,
+            selectValue: 0,
             data: {
                 title: '',
                 image_url: '',
@@ -34,32 +35,62 @@ class Main extends React.Component {
         })
     }
 
+    select = (event) => {
+        event.preventDefault();
+        this.setState({
+            selectValue: Number(event.target.value),
+        })
+    }
+
+
+
     render() {
         return (
             <div>
-                {
-                    Data.map(element => {
-                        return (<Beast title={element.title} image_url={element.image_url} description={element.description} showModal={this.showModal} />)
-                    })
-                }
-                <Modal show={this.state.modal} onHide={this.closeModal}>
-                    <Modal.Header closeButton>
-                        <Modal.Title>
-                            {this.state.data.title}
-                        </Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <img src={this.state.data.image_url} alt={this.state.data.keyword} style={{ width: '100%' }} />
-                        ❤{this.state.data.votes}
-                        <hr />
-                        {this.state.data.description}
-                    </Modal.Body>
-                    <Modal.Footer>
-                        <Button variant="secondary" onClick={this.closeModal}>
-                            Close
-                        </Button>
-                    </Modal.Footer>
-                </Modal>
+                <div>
+                    <p style={{ width: '30%', fontSize: '20px', margin: 'auto' }}>
+                        Select number of horns
+                    </p>
+                    <Form.Select onChange={this.select} style={{ width: '30%', margin: 'auto' }}>
+                        <option value="0">All</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="100">100</option>
+                    </Form.Select>
+                </div>
+                <div >
+                    {
+                        this.state.selectValue === 0 ?
+                        Data.map(element => {
+                            return (<Beast title={element.title}  horns={element.horns} image_url={element.image_url} description={element.description} showModal={this.showModal}/>)
+                        })
+                    :
+                        Data.filter(element => element.horns === this.state.selectValue).map(element => {
+                            return (<Beast title={element.title}  horns={element.horns} image_url={element.image_url} description={element.description} showModal={this.showModal}/>)
+                        })
+                    }
+                </div>
+                <div>
+                    <Modal show={this.state.modal} onHide={this.closeModal}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>
+                                {this.state.data.title}
+                            </Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <img src={this.state.data.image_url} alt={this.state.data.keyword} style={{ width: '100%' }} />
+                            ❤{this.state.data.votes}
+                            <hr />
+                            {this.state.data.description}
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button variant="secondary" onClick={this.closeModal}>
+                                Close
+                            </Button>
+                        </Modal.Footer>
+                    </Modal>
+                </div>
             </div>
         )
     }
